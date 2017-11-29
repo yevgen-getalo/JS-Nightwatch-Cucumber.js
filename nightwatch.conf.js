498e79868419d9c70904ad99abae9caa8e3a513e
yevgen-getalo <yevgen.getalo@gmail.com>
2017-11-29 17:07:04 +0200

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -23,7 +23,8 @@ module.exports = {
     // Sets configuration for Selenium Server
     selenium: {
         start_process: true,
-        //server_path: '/node_modules/selenium-server/lib/runner/selenium-server-standalone-3.7.1.jar',
+        server_path: '/node_modules/selenium-server/lib/runner/selenium-server-standalone-3.7.1.jar',
+        start_session: true
         //chromedriver: chromeDriver.path,
     },
 
